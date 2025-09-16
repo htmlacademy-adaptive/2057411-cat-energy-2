a58df2b51bac91f6b20d787f6e17d5f0f40be9a9
V1Mah <V1Mah@users.noreply.github.com>
2025-09-16 22:35:08 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2057411-cat-energy-2@a74c4786bc468f4d80ef681bfa4e509236ea07c6 🚀

---
+++ 1/scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-nav"),a=document.querySelector(".main-nav__toggle");n.classList.remove("main-nav--nojs");n.classList.add("main-nav--closed");a.addEventListener("click",()=>{n.classList.toggle("main-nav--closed"),n.classList.toggle("main-nav--opened")});
